beabc8706cf717854e9f8ef23f77d9d686a953b0
WindomZ <zwdsdtc@gmail.com>
2016-07-22 01:00:49 +0800

    fix: cache dir

--- cache/cache.go
+++ cache/cache.go
@@ -1,1 +1,8 @@
-package wdcache
+package cache
+
+type ICache interface {
+	SetString(key string, value string, expireSeconds int) error
+	GetString(key string) (string, error)
+	SetInterface(key string, value interface{}, expireSeconds int) error
+	GetInterface(key string, value interface{}) error
+}
--- init.go
+++ init.go
@@ -3,6 +3,6 @@ package wddevkit
 import (
 	_ "github.com/WindomZ/go-develop-kit/cache"
 	_ "github.com/WindomZ/go-develop-kit/cache/freecache"
-	_ "github.com/WindomZ/go-develop-kit/cache/go-cache"
+	_ "github.com/WindomZ/go-develop-kit/cache/gocache"
 	_ "github.com/WindomZ/go-develop-kit/googleauth/otp"
 )
