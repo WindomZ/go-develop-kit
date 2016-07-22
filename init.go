0ef2d8308c1a1cb67e9b2bb2436391cce6d88565
WindomZ <zwdsdtc@gmail.com>
2016-07-22 15:08:20 +0800

    fix: init.go

--- init.go
+++ init.go
@@ -2,7 +2,5 @@ package wddevkit
 
 import (
 	_ "github.com/WindomZ/go-develop-kit/cache"
-	_ "github.com/WindomZ/go-develop-kit/cache/freecache"
-	_ "github.com/WindomZ/go-develop-kit/cache/gocache"
 	_ "github.com/WindomZ/go-develop-kit/googleauth/otp"
 )
