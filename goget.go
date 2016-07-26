06091f8413577a7f62ed22fa2f31e7280ef80922
WindomZ <zwdsdtc@gmail.com>
2016-07-26 14:47:07 +0800

    feat: init unit

--- goget.go
+++ goget.go
@@ -4,4 +4,5 @@ import (
 	_ "github.com/WindomZ/go-develop-kit/cache"
 	_ "github.com/WindomZ/go-develop-kit/googleauth/otp"
 	_ "github.com/WindomZ/go-develop-kit/mutex"
+	_ "github.com/WindomZ/go-develop-kit/unit"
 )
