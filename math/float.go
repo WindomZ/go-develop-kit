fa0158522f7fc62c40a07bed2b25f81d29e81d02
WindomZ <zwdsdtc@gmail.com>
2016-07-29 20:25:59 +0800

    feat: math float sum sub mul div

--- math/float.go
+++ math/float.go
@@ -1,26 +1,6 @@
 package math
 
-import (
-	"github.com/shopspring/decimal"
-	"math"
-)
-
-func FloatPrecision(f float64, prec int, round bool) float64 {
-	pow10_n := math.Pow10(prec)
-	if round {
-		return math.Trunc((f+0.5/pow10_n)*pow10_n) / pow10_n
-	}
-	return math.Trunc((f)*pow10_n) / pow10_n
-}
-
-func FloatRound(val float64, prec int) float64 {
-	pow := math.Pow10(prec)
-	digit := pow * val
-	if _, div := math.Modf(digit); div >= .5 {
-		return math.Ceil(digit) / pow
-	}
-	return math.Floor(digit)
-}
+import "github.com/shopspring/decimal"
 
 func FloatFixed(value float64, places int) float64 {
 	f, _ := decimal.NewFromFloat(value).Round(int32(places)).Float64()
@@ -29,4 +9,48 @@ func FloatFixed(value float64, places int) float64 {
 
 func FloatFixedToInt(value float64, places int) int64 {
 	return decimal.NewFromFloat(value).Mul(decimal.New(1, int32(places))).Round(0).IntPart()
+}
+
+func FloatSumFixed(x, y float64, places int, zs ...float64) float64 {
+	d := decimal.NewFromFloat(x).Add(decimal.NewFromFloat(y))
+	if zs != nil && len(zs) != 0 {
+		for _, z := range zs {
+			d = d.Add(decimal.NewFromFloat(z))
+		}
+	}
+	f, _ := d.Round(int32(places)).Float64()
+	return f
+}
+
+func FloatSubFixed(x, y float64, places int, zs ...float64) float64 {
+	d := decimal.NewFromFloat(x).Sub(decimal.NewFromFloat(y))
+	if zs != nil && len(zs) != 0 {
+		for _, z := range zs {
+			d = d.Sub(decimal.NewFromFloat(z))
+		}
+	}
+	f, _ := d.Round(int32(places)).Float64()
+	return f
+}
+
+func FloatMulFixed(x, y float64, places int, zs ...float64) float64 {
+	d := decimal.NewFromFloat(x).Mul(decimal.NewFromFloat(y))
+	if zs != nil && len(zs) != 0 {
+		for _, z := range zs {
+			d = d.Mul(decimal.NewFromFloat(z))
+		}
+	}
+	f, _ := d.Round(int32(places)).Float64()
+	return f
+}
+
+func FloatDivFixed(x, y float64, places int, zs ...float64) float64 {
+	d := decimal.NewFromFloat(x).Div(decimal.NewFromFloat(y))
+	if zs != nil && len(zs) != 0 {
+		for _, z := range zs {
+			d = d.Div(decimal.NewFromFloat(z))
+		}
+	}
+	f, _ := d.Round(int32(places)).Float64()
+	return f
 }
--- math/float_test.go
+++ math/float_test.go
@@ -2,20 +2,6 @@ package math
 
 import "testing"
 
-func TestFloatPrecision(t *testing.T) {
-	var f float64 = 2.012345
-	if r := FloatPrecision(f, 5, true); r != 2.01235 {
-		t.Fatalf("Error %#v", r)
-	}
-}
-
-func TestFloatRound(t *testing.T) {
-	var f float64 = 2.012345
-	if r := FloatRound(f, 5); r != 2.01235 {
-		t.Fatalf("Error %#v", r)
-	}
-}
-
 func TestFloatFixed(t *testing.T) {
 	var f float64 = 2.012345
 	if r := FloatFixed(f, 5); r != 2.01235 {
@@ -34,6 +20,50 @@ func TestFloatFixedToInt(t *testing.T) {
 	}
 	f = 4.012345
 	if r := FloatFixedToInt(f, 5); r != 401235 {
+		t.Fatalf("Error %#v", r)
+	}
+}
+
+func TestFloatSumFixed(t *testing.T) {
+	const F1 float64 = 1.012345
+	const F2 float64 = 2.012345
+	const F3 float64 = 3.012345
+	const F4 float64 = 4.012345
+	const F5 float64 = 5.012345
+	if r := FloatSumFixed(F1, F2, 5, F3, F4, F5); r != 15.06173 {
+		t.Fatalf("Error %#v", r)
+	}
+}
+
+func TestFloatSubFixed(t *testing.T) {
+	const F1 float64 = 1.012345
+	const F2 float64 = 2.012345
+	const F3 float64 = 3.012345
+	const F4 float64 = 4.012345
+	const F5 float64 = 5.012345
+	if r := FloatSubFixed(F1, F2, 5, F3, F4, F5); r != -13.03704 {
+		t.Fatalf("Error %#v", r)
+	}
+}
+
+func TestFloatMulFixed(t *testing.T) {
+	const F1 float64 = 1.012345
+	const F2 float64 = 2.012345
+	const F3 float64 = 3.012345
+	const F4 float64 = 4.012345
+	const F5 float64 = 5.012345
+	if r := FloatMulFixed(F1, F2, 5, F3, F4, F5); r != 123.41698 {
+		t.Fatalf("Error %#v", r)
+	}
+}
+
+func TestFloatDivFixed(t *testing.T) {
+	const F1 float64 = 1.012345
+	const F2 float64 = 2.012345
+	const F3 float64 = 3.012345
+	const F4 float64 = 4.012345
+	const F5 float64 = 5.012345
+	if r := FloatDivFixed(F1, F2, 5, F3, F4, F5); r != 0.0083 {
 		t.Fatalf("Error %#v", r)
 	}
 }
