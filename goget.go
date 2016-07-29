d68bb18881c5c6765f7294208d85ddccbd34adcb
WindomZ <zwdsdtc@gmail.com>
2016-07-29 15:06:45 +0800

    update goget

--- goget.go
+++ goget.go
@@ -3,6 +3,7 @@ package wddevkit
 import (
 	_ "github.com/WindomZ/go-develop-kit/cache"
 	_ "github.com/WindomZ/go-develop-kit/googleauth/otp"
+	_ "github.com/WindomZ/go-develop-kit/math"
 	_ "github.com/WindomZ/go-develop-kit/mutex"
 	_ "github.com/WindomZ/go-develop-kit/unit"
 )
