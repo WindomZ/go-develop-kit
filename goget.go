e5b5b389740a7a224098285b078b20335e0d7b11
WindomZ <zwdsdtc@gmail.com>
2016-08-11 14:17:27 +0800

    update

--- goget.go
+++ goget.go
@@ -2,8 +2,15 @@ package wddevkit
 
 import (
 	_ "github.com/WindomZ/go-develop-kit/cache"
+	_ "github.com/WindomZ/go-develop-kit/cache/boolcache"
+	_ "github.com/WindomZ/go-develop-kit/cache/numcache"
 	_ "github.com/WindomZ/go-develop-kit/googleauth/otp"
 	_ "github.com/WindomZ/go-develop-kit/math"
 	_ "github.com/WindomZ/go-develop-kit/mutex"
+	_ "github.com/WindomZ/go-develop-kit/netutil"
+	_ "github.com/WindomZ/go-develop-kit/pinyin"
+	_ "github.com/WindomZ/go-develop-kit/security/salt"
+	_ "github.com/WindomZ/go-develop-kit/timeutil"
 	_ "github.com/WindomZ/go-develop-kit/unit"
+	_ "github.com/WindomZ/go-develop-kit/uuid"
 )
