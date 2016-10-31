a265d70c61fcbacd582ea1a32c2bda8174853188
WindomZ <zwdsdtc@gmail.com>
2016-10-31 16:18:24 +0800

    feat: encoding base64 func for bytes

--- security/encoding/base64.go
+++ security/encoding/base64.go
@@ -2,17 +2,39 @@ package encoding
 
 import "encoding/base64"
 
-// EncodeBase64String returns the base64 encoding of data.
-func EncodeBase64String(data string) string {
-	return base64.StdEncoding.EncodeToString([]byte(data))
+// EncodeBase64 returns the base64 bytes encoding of bytes data.
+func EncodeBase64(data []byte) []byte {
+	result := make([]byte, base64.StdEncoding.EncodedLen(len(data)))
+	base64.StdEncoding.Encode(result, data)
+	return result
 }
 
-// DecodeBase64String returns the bytes represented by the base64 string data.
-// if error returns empty string.
+// DecodeBase64 returns the bytes represented by the base64 bytes data.
+func DecodeBase64(data []byte) ([]byte, error) {
+	result := make([]byte, base64.StdEncoding.DecodedLen(len(data)))
+	n, err := base64.StdEncoding.Decode(result, data)
+	return result[:n], err
+}
+
+// EncodeBase64ToString returns the base64 encoding of data.
+func EncodeBase64ToString(data []byte) string {
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+// DecodeBase64FromString returns the bytes represented by the base64 string data.
+func DecodeBase64FromString(data string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(data)
+}
+
+// EncodeBase64String returns the base64 string encoding of string data.
+func EncodeBase64String(data string) string {
+	return EncodeBase64ToString([]byte(data))
+}
+
+// DecodeBase64String returns the string represented by the base64 string data.
 func DecodeBase64String(data string) string {
-	b, err := base64.StdEncoding.DecodeString(data)
-	if err != nil {
-		return ""
+	if b, err := DecodeBase64FromString(data); err == nil {
+		return string(b)
 	}
-	return string(b)
+	return ""
 }
--- security/encoding/base64_test.go
+++ security/encoding/base64_test.go
@@ -1,6 +1,27 @@
 package encoding
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
+
+func TestEncodeBase64(t *testing.T) {
+	var DATA = []byte("This is test!")
+	if data, err := DecodeBase64(EncodeBase64(DATA)); err != nil {
+		t.Fatal(err)
+	} else if !bytes.Equal(data, DATA) {
+		t.Fatal("Fail to encode or decode")
+	}
+}
+
+func TestEncodeBase64ToString(t *testing.T) {
+	var DATA = []byte("This is test!")
+	if data, err := DecodeBase64FromString(EncodeBase64ToString(DATA)); err != nil {
+		t.Fatal(err)
+	} else if !bytes.Equal(data, DATA) {
+		t.Fatal("Fail to encode or decode")
+	}
+}
 
 func TestEncodeBase64String(t *testing.T) {
 	const STR = "This is test!"
