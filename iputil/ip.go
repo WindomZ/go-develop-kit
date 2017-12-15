e9e77eb67201549ed3a9b51a83bd6fee475bf817
WindomZ <git.windomz@gmail.com>
2017-12-15 23:16:39 +0800

    feat(iputil): add ip utils

---
+++ iputil/ip.go
@@ -0,0 +1,53 @@
+package iputil
+
+import (
+	"net"
+	"strings"
+	"sync"
+)
+
+const EmptyIP = "0.0.0.0"
+
+var (
+	once        sync.Once
+	localIP     string
+	localIPTail string
+)
+
+// ip returns the current IP address.
+func ip() (string, error) {
+	if localIP != "" {
+		return localIP, nil
+	}
+	addresses, err := net.InterfaceAddrs()
+	if err != nil {
+		return EmptyIP, err
+	}
+	for _, address := range addresses {
+		if ipNet, ok := address.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				localIP = ipNet.IP.String()
+				return localIP, nil
+			}
+		}
+	}
+	return EmptyIP, nil
+}
+
+// IP returns the current IP address.
+func IP() string {
+	if localIP != "" {
+		return localIP
+	}
+	once.Do(func() {
+		localIP, _ = ip()
+		localIPTail = localIP[strings.LastIndexByte(localIP, '.'):]
+	})
+	return localIP
+}
+
+// IPTail returns the end of the current IP address.
+// like: 4 in 192.168.1.4
+func IPTail() string {
+	return localIPTail
+}
---
+++ iputil/ip_test.go
@@ -0,0 +1,18 @@
+package iputil
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/WindomZ/testify/assert"
+)
+
+func TestIP(t *testing.T) {
+	assert.NotEqual(t, EmptyIP, IP())
+	assert.Regexp(t, regexp.MustCompile(`\d{2,3}\.\d{2,3}\.\d{1,3}\.\d{1,3}`), IP())
+}
+
+func TestIPTail(t *testing.T) {
+	assert.NotEqual(t, "0", IPTail())
+	assert.Regexp(t, regexp.MustCompile(`\d{1,3}`), IPTail())
+}
