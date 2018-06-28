9bf1b4e7f97d5b7183cb43652b7f89baab010b43
WindomZ <git.windomz@gmail.com>
2018-06-28 09:07:24 +0800

    fix: supports uuid library upgrades

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -13,19 +13,27 @@ var (
 
 // NewUUID returns random generated UUID.
 func NewUUID() string {
-	return uuid.NewV4().String()
+	if id, err := uuid.NewV4(); err == nil {
+		return id.String()
+	}
+	return ""
 }
 
 // NewUUIDWithName returns UUID based on SHA-1 hash of namespace UUID and name.
 func NewUUIDWithName(name string) string {
-	return uuid.NewV5(uuid.NewV4(), name).String()
+	if id, err := uuid.NewV4(); err == nil {
+		return uuid.NewV5(id, name).String()
+	}
+	return ""
 }
 
 // NewSafeUUID returns random generated UUID safely.
 func NewSafeUUID() (r string) {
 	mux.Lock()
 	idx++
-	r = uuid.NewV5(uuid.NewV4(), string(idx)).String()
+	if id, err := uuid.NewV4(); err == nil {
+		r = uuid.NewV5(id, string(idx)).String()
+	}
 	mux.Unlock()
 	return
 }
